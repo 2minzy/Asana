45c8eda2eab28f79fab4153f88dd65c69bd8174a
Minji <minjilee0423@gmail.com>
2021-01-05 23:17:33 +0900

    Added TypeGraphQL

---
+++ api/entity/User.ts
@@ -0,0 +1,18 @@
+import { prop as Property, getModelForClass } from '@typegoose/typegoose';
+import { ObjectId } from 'mongodb';
+import { Field, ObjectType } from 'type-graphql';
+
+@ObjectType({ description: 'User' })
+export class User {
+  @Field()
+  readonly _id: ObjectId;
+
+  @Field()
+  @Property({ required: true })
+  email: string;
+
+  @Property({ required: true })
+  password: string;
+}
+
+export const UserModel = getModelForClass(User);
