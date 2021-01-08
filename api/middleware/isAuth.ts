51c1f9b435e4856484b40f27592201055e14f907
Minji <minjilee0423@gmail.com>
2021-01-08 20:45:17 +0900

    Added Apollo server

--- api/middleware/isAuth.ts
+++ api/middleware/isAuth.ts
@@ -2,13 +2,13 @@ import { MiddlewareFn } from 'type-graphql';
 import { MyContext } from '../types/MyContext';
 import jwt from 'jsonwebtoken';
 
-const APP_SECRET = process.env.SESSION_SECRET || 'asofnncnasc3748572';
+const APP_SECRET = process.env.SESSION_SECRET || 'aslkdfjoiq12312';
 
 export const isAuth: MiddlewareFn<MyContext> = async ({ context }, next) => {
   const authorization = context.req.headers['authorization'];
   try {
-    const token = authorization?.replace('Bearer', '');
-    const user = jwt.verify(token!, APP_SECRET) as any;
+    const token = authorization?.replace('Bearer ', '')!;
+    const user = jwt.verify(token, APP_SECRET) as any;
     context.res.locals.userId = user.id;
     return next();
   } catch (err) {
